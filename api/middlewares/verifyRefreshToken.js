8f9ad1e407728acfc1ca12b9459f19633782a303
negativeInteger <139580532+INFERNO1211@users.noreply.github.com>
2025-03-12 22:06:35 +0530

    done ✅

--- api/middlewares/verifyRefreshToken.js
+++ api/middlewares/verifyRefreshToken.js
@@ -3,10 +3,10 @@
  * - Handles token refreshing when access tokens expire.
  */
 import jwt from "jsonwebtoken";
-import { isBlacklisted } from "../services/tokenService.js";
-import { generateAccessToken } from "../utils/tokens.js";
-import { ACCESS_TOKEN_EXPIRE_TIME } from "../config/constants.js";
+import { blacklistToken, isBlacklisted } from "../services/tokenService.js";
+import { generateAccessToken, generateRefreshToken } from "../utils/tokens.js";
 import { ExpressError } from '../errors/ExpressError.js';
+import { setCookies } from "../utils/cookie.js";
 /**
  * Middleware to verify and refresh the access token using the refresh token.
  * - If `req.triggerRefresh` is false, it skips refreshing.
@@ -31,16 +31,16 @@ export const verifyRefreshToken = async (req, res, next) => {
         const user = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
         const blacklisted = await isBlacklisted(refreshToken);
         if (blacklisted) throw new ExpressError('Authentication', 'Something Went Wrong. Please log in again.', 401);
-        const accessToken = generateAccessToken(user);
-        res.cookie("accessToken", accessToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: "Strict",
-            maxAge: ACCESS_TOKEN_EXPIRE_TIME
-        });
+        await blacklistToken(refreshToken);
+        // create new access token
+        const newAccessToken = generateAccessToken(user);
+        // rotate refresh token
+        const newRefreshToken = generateRefreshToken(user);
+        setCookies(res, newAccessToken, newRefreshToken);
         req.user = user;
         next();
     } catch (err) {
         next(err);
     }
 };
+
